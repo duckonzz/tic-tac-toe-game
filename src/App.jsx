a71a619c9e7d7fcee37be792cdde123feef21f7d
Minh Đức <139892190+duckonzz@users.noreply.github.com>
2026-07-16 15:02:28 +0700

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from './assets/vite.svg';
+import heroImg from './assets/hero.png';
+import './App.css';
+import Player from './components/Player';
+
+function App() {
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialname="Player 1" symbol="X" />
+          <Player initialname="Player 2" symbol="O" />
+        </ol>
+        GAME BOARD
+      </div>
+    </main>
+  );
+}
+
+export default App;
